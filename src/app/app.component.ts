f0df9272303f5c0647cb9b49f5ce646caa70dd51
Nitipan Pompan <nitipan.p@hotmail.com>
2021-07-19 10:32:57 +0700

    add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { WebcamImage } from 'ngx-webcam';
+import { Observable, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'testwebcam';
+  output: any = null;
+  private trigger: Subject<void> = new Subject<void>();
+
+  /**
+   *
+   */
+  constructor(private modalService: NgbModal) {}
+  public get triggerObservable(): Observable<void> {
+    return this.trigger.asObservable();
+  }
+  async capture() {
+    this.trigger.next();
+  }
+
+  public handleImage(webcamImage: WebcamImage): void {
+    console.info('received webcam image', webcamImage);
+    this.output = webcamImage.imageAsDataUrl;
+  }
+
+  openWebCam(content: any) {
+    this.modalService
+      .open(content, { ariaLabelledBy: 'modal-basic-title' })
+      .result.then((result) => {
+        //   this.closeResult = `Closed with: ${result}`;
+        // }, (reason) => {
+        //   this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+      });
+  }
 }
---
+++ src/app/live/webcam/webcam.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { WebcamComponent } from './webcam/webcam.component';
+
+@NgModule({
+  declarations: [WebcamComponent],
+  imports: [CommonModule],
+  exports: [WebcamComponent],
+})
+export class WebcamModule {}
